b2d84d5c834919a42be701d2ca3416526f6c0b74
DmitryPolimushket <sidsid007@gmail.com>
2017-04-11 11:03:40 +0300

    initial commit

---
+++ assets/js/common.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+
+    $('.icon').hover(
+        function(){
+            $(this).addClass('hover');
+            $('.hover .front').addClass('hidden'); // for mozilla, IE
+        },
+        function(){
+            $('.hover .front').removeClass('hidden'); // for mozilla, IE
+            $(this).removeClass('hover');
+        });
+
+
+
+        $('.description .button').click(function(){
+            sentForm.elements[0].value = '';
+            $(".pop-up").fadeIn(400);
+        });
+
+        $('.cancel').click(function(){
+            $('.pop-up').hide('slow', function(){
+                $(".pop-up").fadeOut(400);
+            });
+        });
+
+});
